37ea35fea7df86514b5f2002a54f5da2f8765297
isapamumeh <isabellumeh@gmail.com>
2022-07-10 19:06:55 +0100

    pushing

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,22 +1,26 @@
+#include "main.h"
 /**
- * print_sign - prints the sign of a number
- * @n: takes in an integer
- * Return: 1 if n is > zero, 0 if n is zero, -1 if n is < zero
- */
-int print_sign(int n)
+* print_sign - prints the sign of a number
+*
+* @c: number to check
+*
+* Return: +1 if is positive, 0 if is 0 and -1 if is negative
+*/
+int print_sign(int c)
 {
-	if (n > 0)
-	{
-		_putchar('+');
-		return (1);
-	}
-	else if (n == 0)
-	{
-		_putchar('0');
-		return (0);
-	}
-	else
-	{
-		_putchar('-');
-		return (-1);
-	}
+if (c > 0)
+{
+_putchar('+');
+return (1);
+}
+else if (c == 0)
+{
+_putchar('0');
+return (0);
+}
+else
+{
+_putchar('-');
+return (-1);
+}
+}
