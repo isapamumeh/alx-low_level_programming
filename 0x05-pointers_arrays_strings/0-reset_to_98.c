d0af9c583e2e201b92debd3e8401c08f3d54673e
isapamumeh <isabellumeh@gmail.com>
2022-07-13 21:37:03 -0700

    pushing

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,23 @@
+nclude "main.h"
+
+
+
+/**
+ *
+ *  * reset_to_98 - function that takes a pointer to an integer parameter
+ *
+ *   * @n: integer pointer
+ *
+ *    * Return: 0 Success
+ *
+ *     */
+
+
+
+void reset_to_98(int *n)
+
+{
+
+		*n = 98;
+
+}
